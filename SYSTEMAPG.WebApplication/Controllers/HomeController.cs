4844786a52bb957f8c86f03ff150e4bd530022f5
Augusto Gutierrez <augustogutierrez288@gmail.com>
2024-06-07 18:02:11 -0300

    Added templates

--- SYSTEMAPG.WebApplication/Controllers/HomeController.cs
+++ SYSTEMAPG.WebApplication/Controllers/HomeController.cs
@@ -17,11 +17,15 @@ namespace SYSTEMAPG.WebApplication.Controllers
         {
             return View();
         }
-
+        public IActionResult Perfil()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
